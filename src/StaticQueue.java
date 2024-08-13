b529471c54528854ce3d90cf6780c3643ab2b92d
gabri <gabriela@otte.net.br>
2024-08-13 19:14:21 -0300

    commit

---
+++ src/StaticQueue.java
@@ -0,0 +1,49 @@
+public class StaticQueue {
+    private int top = -1;
+    private int base = 0;
+    private int[] data;
+
+    private StaticQueue(int size) {
+        this.data = new int[size];
+    }
+
+    public boolean isFull() {
+        if (top - base == -1) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean isEmpty() {
+        if (top - base == -1) {
+            return true;
+        }
+        return false;
+    }
+
+    public void add(int valor) {
+        if (isFull()) {
+            System.out.println("A fila já está cheia");
+        } else {
+            data[top] = valor;
+            top++;
+        }
+    }
+
+    public int remove() {
+        if (isEmpty()) {
+            System.out.println("Stack cheia");
+            return 0;
+        }
+        data[base] = 0;
+        base++;
+
+    }
+
+    public void clear() {
+        for (int valor : data) {
+            valor = 0;
+        }
+    }
+
+}
